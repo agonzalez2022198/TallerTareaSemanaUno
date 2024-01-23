a57c8fafb072c7b8bfbfaf9b117c82eeb37e1f49
Informatica <informatica@alumno.local>
2024-01-23 16:16:05 -0600

    Push 2

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function mostarMenuEmergente(){
+    var miDiv = document.getElementById('window-notice');
+
+    miDiv.style.display = 'flex';
+}
+
+function mostarMenuEmergente2(){
+    var miDiv = document.getElementById('window-notice2');
+
+    miDiv.style.display = 'flex';
+}
